277ddb2992f4167031ed02f1ec61530238bfb1ec
Babatunde Olaniyi <bolaniyi.mvc@gmail.com>
2021-08-21 12:37:43 -0500

    Confifure services in start.up n create Default connection in appsettings

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,13 +1,14 @@
+using Domain;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext: DbContext
     {
-        public DbSet <Value> Values {get; set; }
-        public DataContext (DbContextOptions options) : base(options)
+        public DbSet<Value> Values { get; set; }
+        public DataContext(DbContextOptions options) : base(options)
         {
-            
+
         }
     }
 }
